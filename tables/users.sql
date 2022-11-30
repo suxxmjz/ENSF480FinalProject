be37c48b9b85f4c74c9ed7838aeb887e0da1efa4
Caroline <92133996+CarolineBasta@users.noreply.github.com>
2022-11-30 02:16:46 -0700

    Create users.sql

---
+++ tables/users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `users` (
+  `Name` varchar(45) DEFAULT NULL,
+  `Address` varchar(45) DEFAULT NULL,
+  `CardNumber` int DEFAULT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Email`)
+);
+
+
+INSERT INTO `users` VALUES ('sukriti','123 uofc',123456, 'sukriti@email.com','password');
+INSERT INTO `users` VALUES ('caroline','345 abc',789101,'caroline@email.com','hello123');
+INSERT INTO `users` VALUES ('mohammed','234 xyz',893452,'mohammed@email.com','ensf');
+INSERT INTO `users` VALUES ('labib', '874 efg', 975124,'labib@email.com','ucalgary');
+
+DESC user;
