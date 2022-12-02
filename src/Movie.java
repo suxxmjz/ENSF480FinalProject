c9c3636d6e53a7b7cb2e3d55ecd1b34eef732a27
Labib <labibafsarahmed02@gmail.com>
2022-12-02 15:56:48 -0700

    Adding files

---
+++ src/Movie.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+
+public class Movie {
+    private String movieName;
+    private String genre;
+    private int runTime;
+    private LocalDate announcement;
+
+
+    public Movie (String movieName, String genre, int runTime, LocalDate announcement){
+        this.movieName = movieName;
+        this.genre = genre;
+        this.runTime = runTime;
+        this.announcement = announcement;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getRunTime() {
+        return runTime;
+    }
+
+    public LocalDate getAnnouncement() {
+        return announcement;
+    }
+
+}
