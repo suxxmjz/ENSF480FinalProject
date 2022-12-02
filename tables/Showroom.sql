43a21588ea9a7dcf9c6276ca1b0eccfd5f120ebc
Labib <labibafsarahmed02@gmail.com>
2022-12-02 16:06:15 -0700

    Fixing my merge mistake

--- tables/Showroom.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Showroom
-    (showroom_ID      CHAR(4)     NOT NULL,
-    theatre_name     VARCHAR(20)  NOT NULL,
-    PRIMARY KEY(showroom_ID, theatre_name),
-    FOREIGN KEY (theatre_name) REFERENCES Theatre(theatre_name)
-    ON DELETE CASCADE       ON UPDATE CASCADE
-    );
--- tables/theatre.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE theatre(
-    theatre_name VARCHAR(50) NOT NULL,
-    PRIMARY KEY(theatre_name),
-    );
-
-INSERT INTO theatre (theatre_name) VALUES ('Cineplex VIP Cinemas University District'), ('Landmark Cinemas 5 Calgary Market Mall');
