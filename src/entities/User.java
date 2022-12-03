8894044cd0649765241120a21e28c48d40550614
Caroline <92133996+CarolineBasta@users.noreply.github.com>
2022-12-02 21:52:58 -0700

    Update User.java
    change user layout

--- src/entities/User.java
+++ src/entities/User.java
@@ -4,55 +4,17 @@ import java.util.*;
 
 
 public class User{
-    private String firstName;
-    private String lastName;
-    private String userAddress;
-    private String userEmail;
-    private String password;
-    private int cardNumber;
-    private ArrayList<Ticket> userTickets;
-    private boolean is_registered;
+    private ArrayList<Ticket> tickets;
 
-    public User (String firstName, String lastName, String userAddress, String userEmail, String password, int cardNumber, boolean is_registered){
-       this.firstName = firstName;
-       this.lastName = lastName;
-       this.userAddress = userAddress;
-       this.userEmail = userEmail;
-       this.password = password;
-       this.cardNumber = cardNumber;
-
+    public User() {
+	tickets = new ArrayList<Ticket>();
     }
-
-    public ArrayList<Ticket> getUserTickets(){
-	return userTickets;
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    
-    public String getLastName(){
-        return lastName;
-    }
-
-    public String getUserAddress(){
-        return userAddress;
-    }
-
-    public String getuserEmail(){
-        return userEmail;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public int getcardNumber(){
-        return cardNumber;
-    }
-
-    public boolean is_registered(){
-        return this.is_registered;
-    }
+	
+    public ArrayList<Ticket> getTickets() {
+	return tickets;
+	}
+	
+    public void clearTicketList() {
+	tickets.clear();
+	}
 }
