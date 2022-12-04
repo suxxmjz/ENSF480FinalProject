890d43aeb2b871881cc77daf426949b950507d76
su <75515505+suxxmjz@users.noreply.github.com>
2022-12-04 03:32:41 -0700

    Update Showtime.java

--- src/entities/Showtime.java
+++ src/entities/Showtime.java
@@ -10,7 +10,7 @@ public class Showtime {
 	   private String movie_name;
 	   private LocalDateTime showing_time;
 	   
-	   public Showtime(String ID, String tName, String mName, LocalDateTime start) {
+	   public Showtime(String ID, String tName, String mName, LocalDateTime start) { //ctor for showtime
 		   this.showroom_ID = ID;
 		   this.theatre_name = tName;
 		   this.movie_name = mName;
@@ -18,39 +18,39 @@ public class Showtime {
 		   
 	   }
 	   
-	   public void setShowRoom(String ID) {
+	   public void setShowRoom(String ID) { //setter for showroom
 		   this.showroom_ID = ID;
 	   }
 	   
-	   public void setTheatre(String name) {
+	   public void setTheatre(String name) { //setter for theatre
 		   this.theatre_name = name;
 	   }
 	   
-	   public void setMovie(String name) {
+	   public void setMovie(String name) { //setter for movie
 		   this.movie_name = name;
 	   }
 	   
-	   public void setshowing(LocalDateTime start) {
+	   public void setshowing(LocalDateTime start) { //setter for showingtime
 		   this.showing_time = start;
 	   }
 	   
-	   public String getShowRoom() {
+	   public String getShowRoom() { //getter for showroom
 		   return this.showroom_ID;
 	   }
 	   
-	   public String getTheatre() {
+	   public String getTheatre() { //getter for theatre
 		   return this.theatre_name;
 	   }
 	   
-	   public String getMovie() {
+	   public String getMovie() { //getter movie
 		   return this.movie_name;
 	   }
 	   
-	   public LocalDateTime getShowingTime() {
+	   public LocalDateTime getShowingTime() { //getter for showtime
 		   return this.showing_time;
 	   }
 	   
-	   public boolean Check72Hours() {
+	   public boolean Check72Hours() { //check for 72 hours before showtime to validate refund 
 		   Duration duration = Duration.between(LocalDateTime.now(), showing_time);
 		   if(duration.toHours() >= 72)
 			   return true;
@@ -58,7 +58,7 @@ public class Showtime {
 			   return false;
 	   }
 	   
-	   boolean getSeatStatus(DatabaseController DB, int SeatNum) {
+	   boolean getSeatStatus(DatabaseController DB, int SeatNum) { //check if seat is available
 		   ArrayList<Seat> temp = DB.getAvailableSeats(this);
 		   for(Seat i : temp) {
 			   if (i.getseatNumber() == SeatNum) {
