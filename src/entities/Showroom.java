15febc2e03271a52e3a22aac584c41be7b16a659
su <75515505+suxxmjz@users.noreply.github.com>
2022-12-04 03:32:24 -0700

    Update Showroom.java

--- src/entities/Showroom.java
+++ src/entities/Showroom.java
@@ -1,38 +1,33 @@
 package entities;
 
-import java.sql.*;
-import java.util.Arrays;
-import java.util.List;
-
 public class Showroom {
 	public  String dbURL;
 	public  String username;
 	public  String password;
    
-   private Connection dbConnect;
-   private ResultSet results;
+
    
    private String showroom_ID;
    private String theatre_name;
    
-   public Showroom(String ID, String Name) {
+   public Showroom(String ID, String Name) { //ctor for showroom
 	   this.showroom_ID = ID;
 	   this.theatre_name = Name;
    }
    
-   public void setShowRoom(String ID) {
+   public void setShowRoom(String ID) { //setter for showroom
 	   this.showroom_ID = ID;
    }
    
-   public void settheatre(String name) {
+   public void settheatre(String name) { //setter for theatre
 	   this.theatre_name = name;
    }
    
-   public String getShowRoom() {
+   public String getShowRoom() { //getter for showroom
 	   return this.showroom_ID;
    }
    
-   public String getTheatre() {
+   public String getTheatre() { //getter for theatre
 	   return this.theatre_name;
    }
 }
