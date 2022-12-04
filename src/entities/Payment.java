eb57160eeba2e2d1c0579f7058335f8b2ed54f6e
Caroline <92133996+CarolineBasta@users.noreply.github.com>
2022-12-04 03:39:42 -0700

    Update Payment.java

--- src/entities/Payment.java
+++ src/entities/Payment.java
@@ -6,6 +6,8 @@ public class Payment {
     private int cardNumber;
     private boolean refunded;
 
+    
+   //constructor
     public Payment(int ticketID, float price, String email, int cardNumber, boolean refunded){
         this.ticketID = ticketID;
         this.price = price;
@@ -15,22 +17,28 @@ public class Payment {
 
     }
 
+    //getter for payment ID
     public int getID(){
         return ticketID;
     }
-
+    
+    //getter for movie price
     public float getPrice(){
         return price;
     }
-
+     
+    //getter for email
     public String getEmail(){
         return email;
     }
-
+    
+    //getter for user's card number
     public int getCardNumber(){
         return cardNumber;
     }
-
+    
+    
+    //a boolean flag to check if the amount has been refunded when user cancels
     public boolean checkRefunded(){
         return refunded;
     }
