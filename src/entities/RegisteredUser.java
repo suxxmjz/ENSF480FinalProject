df4a20e5a108c47d9e55399930e82f1f847e7073
Caroline <92133996+CarolineBasta@users.noreply.github.com>
2022-12-03 18:03:18 -0700

    Update RegisteredUser.java

--- src/entities/RegisteredUser.java
+++ src/entities/RegisteredUser.java
@@ -1,47 +1,7 @@
 package entities;
-import java.util.*;
-
 
 public class RegisteredUser extends User {
-            private String firstName;
-            private String lastName;
-            private String userAddress;
-            private String userEmail;
-            private String password;
-            private int cardNumber;
- 
             public RegisteredUser(String firstName, String lastName, String userAddress, String userEmail, String password, int cardNumber) {
-                this.firstName = firstName;
-                this.lastName = lastName;
-                this.userAddress = userAddress;
-                this.userEmail = userEmail;
-                this.password = password;
-                this.cardNumber = cardNumber;          
-            }
-
-            
-                public String getFirstName(){
-                    return firstName;
-                }
-            
-                
-                public String getLastName(){
-                    return lastName;
-                }
-            
-                public String getUserAddress(){
-                    return userAddress;
-                }
-            
-                public String getuserEmail(){
-                    return userEmail;
-                }
-            
-                public String getPassword(){
-                    return password;
-                }
-            
-                public int getcardNumber(){
-                    return cardNumber;
-                }
+                super(firstName, lastName, userAddress, userEmail, password, cardNumber);            
+            }  
 }
