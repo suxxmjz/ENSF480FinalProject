d8cd460e48206e27aa8ef4a45a0b1e1c467ef422
Caroline <92133996+CarolineBasta@users.noreply.github.com>
2022-12-04 03:34:27 -0700

    Update Movie.java

--- src/entities/Movie.java
+++ src/entities/Movie.java
@@ -11,30 +11,35 @@ public class Movie {
     private int runTime;
     private LocalDate announcement;
 
-
+   //constructor
     public Movie (String movieName, String genre, int runTime, LocalDate announcement){
         this.movieName = movieName;
         this.genre = genre;
         this.runTime = runTime;
         this.announcement = announcement;
     }
-
+    
+	//getter for movie name
     public String getMovieName() {
         return movieName;
     }
-
+	
+   //getter for movie genre
     public String getGenre() {
         return genre;
     }
-
+ 
+   //getter for movie run time
     public int getRunTime() {
         return runTime;
     }
 
+	//getter for annoucement that uses the local date of the user
     public LocalDate getAnnouncement() {
         return announcement;
     }
-    
+      
+	//function to give movie annoucements one week before they are publicly released
     public boolean Check1Week() {
 		   Duration duration = Duration.between(LocalDate.now(), announcement);
 		   if(duration.toDays() <= 7)
