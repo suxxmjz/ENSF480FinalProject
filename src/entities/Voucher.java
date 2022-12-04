8eaf300fc9997cdfb935c54b9fae71462c0d3004
Caroline <92133996+CarolineBasta@users.noreply.github.com>
2022-12-04 03:45:39 -0700

    Update Voucher.java

--- src/entities/Voucher.java
+++ src/entities/Voucher.java
@@ -8,6 +8,7 @@ public class Voucher{
 	private LocalDate expirationDate;
 	private String email;
 
+	//constructor 
 	Voucher(double amount, LocalDate expirationDate, String email){
 		int max = 1000000;
 		int min = 10000;
@@ -18,34 +19,42 @@ public class Voucher{
 		this.setEmail(email);
 	}
 
+	//getter for voucher code
 	public int getCode() {
 		return code;
 	}
 
+	//setter for voucher code
 	public void setCode(int code) {
 		this.code = code;
 	}
-
+	
+        //getter for voucher amount
 	public double getAmount() {
 		return amount;
 	}
-
+	
+       //setter for voucher amount
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
 
+	//getter for expiration date that that uses user's local date
 	public LocalDate getExpirationDate() {
 		return expirationDate;
 	}
 
+	//setter for expiration date
 	public void setExpirationDate(LocalDate expirationDate) {
 		this.expirationDate = expirationDate;
 	}
-
+	
+	//getter for email
 	public String getEmail() {
 		return email;
 	}
 
+	//setter email
 	public void setEmail(String email) {
 		this.email = email;
 	}
