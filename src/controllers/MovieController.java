ceb0951c7097f1a98f15c318d439dc53fc089dfd
su <75515505+suxxmjz@users.noreply.github.com>
2022-12-05 19:18:49 -0700

    Update MovieController.java

--- src/controllers/MovieController.java
+++ src/controllers/MovieController.java
@@ -8,14 +8,14 @@ import gui.WelcomeGUI;
 public class MovieController {
                
         public static void main(String[] args) {
-            DatabaseController dbControl = new DatabaseController();
-            MovieController app = new MovieController();
-            CancellationController canControl = new CancellationController(dbControl);
-            PaymentController payControl = new PaymentController(dbControl);
+            DatabaseController dbControl = new DatabaseController(); //establish database
+            MovieController app = new MovieController(); //establish moviecontroller
+            CancellationController canControl = new CancellationController(dbControl); //establish cancellation controller
+            PaymentController payControl = new PaymentController(dbControl); //establish payment controller
             
                 try {
                     UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                    WelcomeGUI window = new WelcomeGUI(app, dbControl, canControl, payControl);
+                    WelcomeGUI window = new WelcomeGUI(app, dbControl, canControl, payControl); //open welcome gui
                     window.frame.setVisible(true);
                 } catch (Exception e){
                     System.out.println(e);
