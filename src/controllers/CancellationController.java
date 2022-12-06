54cfc9b76518d2d20e1ef1e8ae1a778934fd9050
su <75515505+suxxmjz@users.noreply.github.com>
2022-12-05 19:14:30 -0700

    Update CancellationController.java

--- src/controllers/CancellationController.java
+++ src/controllers/CancellationController.java
@@ -18,39 +18,39 @@ public class CancellationController {
     private Voucher newV;
 
 
-    public CancellationController(DatabaseController db) {
+    public CancellationController(DatabaseController db) { //set database instance
         this.db = db;
     }
 
-    public void setApp(MovieController incoming) {
+    public void setApp(MovieController incoming) { //set moviecontroller instance
         app = incoming;
     }
 
 
-    public void validateTicket(int id, String email) {
+    public void validateTicket(int id, String email) { //function to check if ticket is able to be cancelled
        
     
         ticket = db.getTicket(id, email);
 
-        if (ticket == null) {
+        if (ticket == null) { //if ticket is invalid
             System.out.print("Ticket does not exist.");
             return;
         }
 
-        if (!check72hours(ticket)) {
+        if (!check72hours(ticket)) { //refund window(72 hours prior) has passed
             System.out.print("Refund failed. Please refund 72 hours prior to showtime.");
             return;
         }
 
 
-        boolean checkReg = db.checkRegisterStatus(email);
+        boolean checkReg = db.checkRegisterStatus(email); //check registration status of user
         if(checkReg){
           
 			LocalDate expDate = LocalDate.now();
 			int max = 1000000;
 			int min = 10000;
 			int range = max - min + 1;
-			int theCode =(int)(Math.random() * range) + min;
+			int theCode =(int)(Math.random() * range) + min; //generate voucher id
             newV = new Voucher(theCode, 20, expDate, email);
         }
         else{
@@ -59,18 +59,18 @@ public class CancellationController {
             int min = 10000;
             int range = max - min + 1;
             int theCode = (int) (Math.random() * range) + min;
-            newV = new Voucher(theCode, 20, expDate, email);
+            newV = new Voucher(theCode, 17, expDate, email);
         }
 
         Showtime theShowTime = ticket.getShowtimeObj();
         int theSeat = ticket.getSeatNo();
 
-        db.changeToAvailable(theShowTime, theSeat);
-        db.addVoucher(newV);
+        db.changeToAvailable(theShowTime, theSeat); //change seat back to available
+        db.addVoucher(newV); //add voucher info to database
   
     }
 
-    private boolean check72hours(Ticket ticket) {
+    private boolean check72hours(Ticket ticket) { //check if 72 hours window has passed for refund
         Duration duration = Duration.between(LocalDateTime.now(), (ticket.getShowtimeObj()).getShowingTime());
         if (duration.toHours() >= 72)
             return true;
