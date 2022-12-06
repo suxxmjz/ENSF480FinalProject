853fa550c580ddfdca20bf11b39a22b328113da6
su <75515505+suxxmjz@users.noreply.github.com>
2022-12-05 19:17:33 -0700

    Update PaymentController.java

--- src/controllers/PaymentController.java
+++ src/controllers/PaymentController.java
@@ -12,26 +12,26 @@ public class PaymentController {
     private MakePaymentGUI payTabO;
     private User currUser;
     
-    public PaymentController(DatabaseController db){
+    public PaymentController(DatabaseController db){ //set database instance
         this.dbControl = db;
     }
     
     
-    public void setApp(MovieController app){
+    public void setApp(MovieController app){ //set movie controller instance
         this.app = app;
     }
 
-    public void makePayment(User user, Ticket theT, Showtime theTime){
+    public void makePayment(User user, Ticket theT, Showtime theTime){ //to make a payment
         currUser = user;
         String eml = currUser.getEmail();
         Showtime theShowTime = theT.getShowtimeObj();
         int theSeat = theT.getSeatNo();
-        boolean reg = dbControl.checkRegisterStatus(eml);
+        boolean reg = dbControl.checkRegisterStatus(eml); //check registration status of user
         if(reg){
-            payTabR = new PaymentGUI(dbControl, theTime, theSeat, theT.getID(), eml);
+            payTabR = new PaymentGUI(dbControl, theTime, theSeat, theT.getID(), eml); //open gui that has user and card info saved
         }
         else{
-            payTabO = new MakePaymentGUI(dbControl, theTime, theSeat, theT.getID(), eml);
+            payTabO = new MakePaymentGUI(dbControl, theTime, theSeat, theT.getID(), eml); //open gui where user has to enter their info
         }
         currUser.newTicket(theT);
         dbControl.changeToBooked(theShowTime, theSeat);
